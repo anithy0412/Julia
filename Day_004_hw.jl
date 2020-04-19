9cc59a6fba15661e7509f673583c65dfab918978
anithy0412 <62243333+anithy0412@users.noreply.github.com>
2020-04-19 20:01:54 +0800

    Add files via upload

---
+++ Day_004_hw.jl
@@ -0,0 +1,14 @@
+
+println("$(rpad(Int128,7)):Min $(typemin(Int128)), Max $(typemax(Int128))")
+println("$(rpad(Int32,7)): Min $(typemin(Int32)), Max $(typemax(Int128))")
+println("$(rpad(Int16,7)): Min $(typemin(Int8)), Max $(typemax(Int128))")
+println("$(rpad(Int8,7)): Min $(typemin(Int8)), Max $(typemax(Int8))")
+println("$(rpad(UInt128,7)): Min $(typemin(UInt128)), Max $(typemax(UInt128))")
+println("$(rpad(UInt32,7)): Min $(typemin(UInt32)), Max $(typemax(UInt128))")
+println("$(rpad(UInt16,7)): Min $(typemin(UInt8)), Max $(typemax(UInt128))")
+println("$(rpad(UInt8,7)): Min $(typemin(UInt8)), Max $(typemax(UInt8))")
+
+println("$(rpad(Float32, 7)): Min $(typemin(Float32)), Max $(typemax(Float32))")
+println("$(rpad(Float16, 7)): Min $(typemin(Float16)), Max $(typemax(Float16))")
+
+
